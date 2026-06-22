b9842275801a5e1d243a3a39896852fdc8a1b29f
Aryan Kumar <136561303+Lokasorus@users.noreply.github.com>
2026-06-22 20:48:31 +0530

    Time: 136 ms (84.6%), Space: 144 MB (91.9%) - LeetHub

--- 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
+++ 0220-contains-duplicate-iii/0220-contains-duplicate-iii.cpp
@@ -1,21 +1,38 @@
 class Solution {
 public:
+long long getBucket(int x, int size){
+    if(x>=0) return x/size;
+    return ((long long) x+1)/size-1;
+}
     bool containsNearbyAlmostDuplicate(vector<int>& nums, int indexDiff, int valueDiff) {
  set<int> st;
         int i = 0;
         int n = nums.size();
+        int bucketSize = (long long)valueDiff+1;
+        unordered_map<int, int> bucket;
+
         while(i<n){
             long long x = nums[i];
-            auto it = st.lower_bound(x-valueDiff);
-            if(it!=st.end() && *it<=x+valueDiff){
+            int j = getBucket(nums[i], bucketSize);
+            if(bucket.count(j)){
                 return true;
             }
-            st.insert(x);
+            if(bucket.count(j-1) && abs(bucket[j-1] - x) <= valueDiff){
+                return true;
+            }
+
+            if(bucket.count(j+1) && abs(bucket[j+1] - x) <= valueDiff){
+                return true;
+            }
+            bucket[j] = x;
             if(i>=indexDiff){
-                st.erase(nums[i-indexDiff]);
+                int k = getBucket(nums[i-indexDiff], bucketSize);
+                bucket.erase(k);
             }
             i++;
         }
         return false;
+
+            
     }
 };
