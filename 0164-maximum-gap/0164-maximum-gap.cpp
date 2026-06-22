390d2009c851a625aab2ae6cdbb233bea141c368
Aryan Kumar <136561303+Lokasorus@users.noreply.github.com>
2026-06-22 17:59:20 +0530

    Time: 157 ms (5.06%), Space: 135.5 MB (5.23%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
-void countSort(vector<int> &nums, int n, int exp){
-    vector<int> ans(n);
-    vector<long long> cnt(10, 0);
-    for(int i = 0; i<nums.size(); i++){
-        cnt[(nums[i]/exp)%10]++;
-    }
-    for(int i = 1; i<10; i++){
-        cnt[i]+=cnt[i-1];
-    }
-    for(int i = n-1; i>=0; i--){
-        ans[cnt[(nums[i]/exp)%10] -1] = nums[i];
-        cnt[(nums[i]/exp)%10]--;
-    }
-    for(int i = 0; i<n; i++){
-        nums[i] = ans[i];
-    }
-}
-void radixSort(vector<int>& nums, int n){
+void bucketSort(vector<int>& nums, int n){
     int maxi = *max_element(nums.begin(), nums.end());
-    for(long long exp = 1; maxi/exp>0; exp*=10){
-        countSort(nums, n, exp);
+    int mini = *min_element(nums.begin(), nums.end());
+    int Range = (maxi -mini)/n +1;
+    vector<vector<int>> buckets(n);
+    for(int i = 0; i<n; i++){
+        int idx = (nums[i]-mini)/Range;
+        buckets[idx].push_back(nums[i]);
     }
+
+    int k = 0;
     
+    for(int i = 0; i<n; i++){
+        sort(buckets[i].begin(), buckets[i].end());
+        for(int x: buckets[i]){
+            nums[k++] = x;
+
+        }
+    }
 }
     int maximumGap(vector<int>& nums) {
         int n = nums.size();
-        radixSort(nums, n);
+        bucketSort(nums, n);
         int maxi =0;
         for(int i = 0; i<nums.size()-1; i++){
             maxi = max(maxi, nums[i+1]-nums[i]);
