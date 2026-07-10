532f333b524dbdbc052d95b3666c6580f07344b7
Aryan Kumar <136561303+Lokasorus@users.noreply.github.com>
2026-07-10 17:27:47 +0530

    Time: 2730 ms (5%), Space: 69.2 MB (46.8%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,16 +1,51 @@
 class Solution {
 public:
-    int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-        for(int i = 0; i<k; i++){
-            pq.push(nums[i]);
-        }
-        for(int i = k; i<nums.size(); i++){
-            if(nums[i] > pq.top()){
-                pq.pop();
-                pq.push(nums[i]);
+    int randomIndex(int left, int right) {
+        int len = right - left + 1;
+        return (rand() % len) + left;
+    }
+
+    int partitionAndReturnIndex(vector<int>& nums, int pivotIndex,
+                                int left, int right) {
+        int pivot = nums[pivotIndex];
+
+        swap(nums[left], nums[pivotIndex]);
+
+        int ind = left + 1;
+
+        for (int i = left + 1; i <= right; i++) {
+            if (nums[i] > pivot) {
+                swap(nums[ind], nums[i]);
+                ind++;
             }
         }
-        return pq.top();
+
+        swap(nums[left], nums[ind - 1]);
+
+        return ind - 1;
+    }
+
+    int findKthLargest(vector<int>& nums, int k) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (true) {
+            int pivotIndex = randomIndex(left, right);
+
+            pivotIndex = partitionAndReturnIndex(
+                nums, pivotIndex, left, right
+            );
+
+            if (pivotIndex == k - 1)
+                return nums[pivotIndex];
+
+            else if (pivotIndex > k - 1)
+                right = pivotIndex - 1;
+
+            else
+                left = pivotIndex + 1;
+        }
+
+        return -1;
     }
 };
