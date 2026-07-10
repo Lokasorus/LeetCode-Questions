d6a6e68337be082e0f722d0bc86e00f53907bfc1
Aryan Kumar <136561303+Lokasorus@users.noreply.github.com>
2026-07-10 23:45:13 +0530

    Time: 49 ms (11.67%), Space: 47.8 MB (10.92%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -2,19 +2,38 @@ class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
 
-        vector<int> freq(26, 0);
+        unordered_map<char, int> mpp;
         for(int i = 0; i<tasks.size(); i++){
-            freq[tasks[i] - 'A']++;
+            mpp[tasks[i]]++;
         }
+        priority_queue<int> pq;
+        for(auto it: mpp){
+            pq.push(it.second);
+        }
+        int time = 0;
 
-        int maxi  = *max_element(freq.begin(), freq.end());
-        int cnt = 0;
-        for(auto it: freq){
-            if(it == maxi){
-                cnt++;
+        while(!pq.empty()){
+            vector<int> temp;
+            int cycle = n+1;
+            int i = 0;
+            while(i<cycle && !pq.empty()){
+                i++;
+                int cnt = pq.top();
+                pq.pop();
+                cnt--;
+                if(cnt>0) temp.push_back(cnt);
+                time++;
+
             }
-        }
+            for(auto it: temp){
+                pq.push(it);
+            }
 
-        return max((int)tasks.size(), (maxi-1)*(n+1)+cnt);
+            if(pq.empty()) break;
+
+            time+= cycle - i;
+
+        }
+        return time;
     }
 };
