3ff508cd1e310f55dd5af5d7dbab69a2eacb055d
Aryan Kumar <136561303+Lokasorus@users.noreply.github.com>
2026-07-20 21:34:59 +0530

    Time: 11 ms (38.99%), Space: 94.4 MB (42.77%) - LeetHub

---
+++ 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<int> goodDaysToRobBank(vector<int>& security, int time) {
+        int n = security.size();
+        vector<int> inc(n, 1);
+        vector<int> dec(n, 1);
+        int in = 1;
+        int de = 1;
+        for(int i = 1; i<security.size(); i++){
+            if(security[i]<=security[i-1]){
+                in++;
+                inc[i] = in;
+            }else{
+                in = 1;
+            }
+        }
+        for(int i = n-2; i>=0; i--){
+            if(security[i]<=security[i+1]){
+                de++;
+                dec[i] = de;
+            }else{
+                de = 1;
+            }
+        }
+        vector<int> ans;
+        for(int i = 0; i<n; i++){
+            if(inc[i]-1>=time && dec[i]-1>=time){
+                ans.push_back(i);
+            }
+
+        }
+        return ans;
+        
+    }
+};
